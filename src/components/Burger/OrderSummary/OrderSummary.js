e7e5e81c981777b807cd3a0cb2c10890f5a6bfca
alamgir hossain <alamgir555hossain@gmail.com>
2020-08-01 21:17:25 -0400

    navigation, sidedrawer, backdrop

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Ax';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+    componentDidUpdate(){
+        console.log('[OrderSummary] did update');
+    }
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}</li>
+            );
+        });
+        
         return (
-            <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}</li>
-        );
-    });
-    return (
-        <Aux>
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients: </p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseChancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseChancelled}>Cancel</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
         </Aux>
-    );
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
